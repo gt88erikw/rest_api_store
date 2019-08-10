45c8df6a34c06f9b93cea1c6aa8376d7c7499758
gt88erikw <44692311+gt88erikw@users.noreply.github.com>
2019-08-10 14:35:45 -0500

    First commit REST api access items, stores and users with authentication.

---
+++ src/resources/store.py
@@ -0,0 +1,35 @@
+from flask_restful import Resource
+from src.models.store import StoreModel
+
+
+class Store(Resource):
+    def get(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            return store.json() # default 200 is returned here when we don't specify it
+        return {'message': 'Store not found'}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': f'A store named {name} already exists.'}, 400
+
+        store = StoreModel(name)
+        try:
+            store.save_to_db()
+        except:
+            return {'message': 'An error occurred while creating the store.'}, 500
+
+        return store.json(), 201
+
+    def delete(self, name):
+        store = StoreModel.find_by_name(name)
+        if store:
+            store.delete_from_db()
+
+        return {'message': 'Store deleted'}
+
+
+class StoreList(Resource):
+    def get(self):
+        # could use list(map(lambda x: x.json(), StoreModel.query.all())) instead of comprehension
+        return {'stores': [store.json() for store in StoreModel.query.all()]}
