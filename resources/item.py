df24ffce45b891a952b9ef7b04f6f49e2d79c7b4
Erik Warren <gt88erikw@yahoo.com>
2019-09-08 22:14:48 -0500

    logging out using blacklist this commit is the end of the beginner udemy course

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,11 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import (
+    jwt_required,
+    get_jwt_claims,
+    jwt_optional,
+    get_jwt_identity,
+    fresh_jwt_required
+)
 from models.item import ItemModel
 
 
@@ -23,6 +29,7 @@ class Item(Resource):
             return item.json()
         return {'message': 'Item not found'}, 404
 
+    @fresh_jwt_required
     def post(self, name):
         if ItemModel.find_by_name(name):
             return {'message': f"An item with name '{name}' already exists."}, 400
@@ -68,6 +75,14 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
         # could use list(map(lambda x: x.json(), ItemModel.query.all())) instead of comprehension
-        return {'items': [item.json() for item in ItemModel.find_all()]}
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'more data available if logged in.'
+        }, 200
--- resources/user.py
+++ resources/user.py
@@ -1,7 +1,13 @@
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_refresh_token_required,
+    get_jwt_identity,
+    jwt_required, get_raw_jwt)
 from flask_restful import Resource, reqparse
 from werkzeug.security import safe_str_cmp
 
+from blacklist import BLACKLIST
 from models.user import UserModel
 
 _user_parser = reqparse.RequestParser()
@@ -65,6 +71,19 @@ class UserLogin(Resource):
 
         return {'message': 'Invalid credentials'}, 401
 
-        # create access token
-        # create refresh token
-        # return them
+
+class UserLogout(Resource):
+    @jwt_required
+    def post(self):
+        jti = get_raw_jwt()['jti']  # jti is JWT ID, a unique identifier for a jwt
+        BLACKLIST.add(jti)
+        return {'message': 'Successfully logged out'}, 200
+
+
+
+class TokenRefresh(Resource):
+    @jwt_refresh_token_required
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        return {'access_token': new_token}, 200
